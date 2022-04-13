46bbcd7531e3811f2abd2f52d52627692b562a5a
Abhiveer773 <90145630+Abhiveer773@users.noreply.github.com>
2022-04-13 23:08:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+setTimeout(function()
+{img_id = "selfie1";
+take_snapshot();
+speak_data = "taking my selfie in 10 seconds";
+var utterThis  = new SpeechSynthesisUtterance(speck_data);
+synth.speak(utterThis);
+}; 5000 );
+
+function setTimeout()
+{
+    console.log(img_id);
+
+    webcam.snap(function(data_url)
+    {
+if(img_id == "selfie1")
+{
+    document.getElementById("result1").innerHTML = '<img id="selfie1" src" '+data_uri+' "/>';
+}
+if(img_id == "selfie2")
+{
+    document.getElementById("result2").innerHTML = '<img id="selfie2" src" '+data_uri+' "/>';
+}
+if(img_id == "selfie2")
+{
+    document.getElementById("result2").innerHTML = '<img id="selfie2" src" '+data_uri+' "/>';
+}
+    });
+}
